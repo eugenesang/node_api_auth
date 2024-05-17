3db6b4c7e027517a04f4eeb88afaff0ed7e8cede
Eugene Sang <eugenekipropsangtg@gmail.com>
2024-05-17 10:29:31 +1000

    server update

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ require('dotenv').config();
 const express = require("express");
 const cors = require("cors");
 const route = require("./router");
+const logsMiddleware = require("./middleware/log");
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cors({origin: '*'}));
+
+app.use(logsMiddleware);
+
 
 route(app);
 
